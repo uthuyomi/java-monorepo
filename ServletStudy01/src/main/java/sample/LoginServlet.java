8363515d64aa9dbfe6504f1686b1e9cd65771912
yassann20 <kaiseif4e@gmail.com>
2025-08-12 00:48:17 +0900

    study01完成

---
+++ ServletStudy01/src/main/java/sample/LoginServlet.java
@@ -0,0 +1,47 @@
+package sample;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		HttpSession session = request.getSession();
+		if("admin".equals(username) || "1234".equals(password) ) {
+			session.setAttribute("loginUser", username);
+			response.sendRedirect("login.jsp");
+		}else {
+			session.removeAttribute("loginUser");
+			response.sendRedirect("login.jsp?error=1");
+		}
+	}
+
+}
