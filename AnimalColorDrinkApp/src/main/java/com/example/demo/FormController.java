97518b07785f9622dbb40e58132862ee81d58f91
uthuyomi <kaiseif4e@gmail.com>
2025-09-04 22:42:28 +0900

    学習中

--- AnimalColorDrinkApp/src/main/java/com/example/demo/FormController.java
+++ AnimalColorDrinkApp/src/main/java/com/example/demo/FormController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,14 +14,20 @@ public class FormController {
 		return "/form";
 	}
 	
-	@PostMapping("/post")
+	@PostMapping("/result")
 	public String resultForm(
 			@RequestParam(name="animal", required=false) String animal,
 			@RequestParam(name="color", required=false) String color,
 			@RequestParam(name="drink", required=false) String drink,
 			Model model
 			) {
-		
-		
+		if( animal == null || animal.trim().isEmpty() || color == null || color.trim().isEmpty() || drink == null || drink.trim().isEmpty() ) {
+			model.addAttribute("message", "まだ入力されていません");
+		} else {
+			model.addAttribute("animal", animal);
+			model.addAttribute("color", color);
+			model.addAttribute("drink", drink);
+		}
+		return "/resultForm";
 	}
 }
